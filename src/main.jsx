6aca84bf43836218e927caea5c857a39ee86d771
Cato <catriel.dapice@devborn.com>
2023-07-20 16:37:56 -0300

    testing deploy v5

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { RecurlyProvider } from "@recurly/react-recurly";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <React.StrictMode>
     <RecurlyProvider publicKey="sandbox_ndz7j27w_6kjmvscwhbbrw5mm">
       <App />
     </RecurlyProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
