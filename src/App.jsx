817275c4ee2e1f74f19ec760de6a43616c285fbe
Cato <catriel.dapice@devborn.com>
2023-07-21 16:23:00 -0300

    testing deploy v7

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,29 @@ import { Elements } from "@recurly/react-recurly";
 function App() {
   const recurlyTest = () => {
     const handleClick = () => {
-      window.recurly.configure("ewr1-q6rMPJ6ob6ZUZyffxfyevh");
-      const paypal = window.recurly.PayPal({
-        display: {
-          displayName: "Test",
-          description: "This is difficult",
-        },
-      });
-      console.log(paypal);
-      paypal.on("error", (error) => {
-        alert(error);
-        console.log(error, "error");
-      });
-      paypal.on("token", (token) => {
-        alert(token);
-        console.log(token, "token");
-        const urlWithToken = `/payer_id=${token.payer_id}?id=${token.id}`;
-        window.location = `fabfitfunApp://fff/${urlWithToken}`;
-        alert(window.location);
-        console.log(window.location, "window.location");
-      });
-      paypal.start();
+      window.location = `fabfitfunApp://fff`;
+
+      // window.recurly.configure("ewr1-q6rMPJ6ob6ZUZyffxfyevh");
+      // const paypal = window.recurly.PayPal({
+      //   display: {
+      //     displayName: "Test",
+      //     description: "This is difficult",
+      //   },
+      // });
+      // console.log(paypal);
+      // paypal.on("error", (error) => {
+      //   alert(error);
+      //   console.log(error, "error");
+      // });
+      // paypal.on("token", (token) => {
+      //   alert(token);
+      //   console.log(token, "token");
+      //   const urlWithToken = `/payer_id=${token.payer_id}?id=${token.id}`;
+      //   window.location = `fabfitfunApp://fff/${urlWithToken}`;
+      //   alert(window.location);
+      //   console.log(window.location, "window.location");
+      // });
+      // paypal.start();
       return;
     };
 
@@ -38,7 +40,7 @@ function App() {
 
   return (
     <div>
-      <h1>react-recurly demo v6</h1>
+      <h1>react-recurly demo v7</h1>
       <Elements>{recurlyTest()}</Elements>
     </div>
   );
